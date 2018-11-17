7474236248e8f2a38b9196185c9570ce8f6b2d63
Shivank <shivankkapoor5@gmail.com>
2018-11-18 01:02:47 +0530

    Update JAVAp.java

--- JAVAp.java
+++ JAVAp.java
@@ -6,14 +6,13 @@ public class JAVAp extends JFrame{
 	 JComboBox jcb  = new JComboBox();
 	 JComboBox jcbFontSize  = new JComboBox();
   public JAVAp(){
-       setLayout(new FlowLayout());
+       JPanel j1 =new JPanel(new GridLayout(1,4));
        JMenu menu1 =new JMenu("File");
        JMenu menu2 =new JMenu("Edit");
        JMenu menu3 =new JMenu("Insert");
-       
        JMenu submenu1 =new JMenu("Type");
-       JMenuBar mb = new JMenuBar();
-       
+       JMenu submenu2 =new JMenu("Case");
+       JMenuBar mb = new JMenuBar();       
        JMenuItem i1= new JMenuItem("Open");
        JMenuItem i2= new JMenuItem("Save");
        JMenuItem i3= new JMenuItem("SaveAs");
@@ -27,7 +26,8 @@ public class JAVAp extends JFrame{
        JMenuItem i11= new JMenuItem("Circle");
        JMenuItem i12= new JMenuItem("Triangle");
        JMenuItem i13= new JMenuItem("Octagon");
-       
+       JMenuItem i14= new JMenuItem("Upper");
+       JMenuItem i15= new JMenuItem("Lower");       
        menu1.add(i1);
        menu1.add(i2);
        menu1.add(i3);
@@ -41,20 +41,22 @@ public class JAVAp extends JFrame{
        menu3.add(i11);
        menu3.add(i12);
        menu3.add(i13);
-       
+       menu2.add(i14);
+       menu2.add(i15);
        mb.add(jcb);
        mb.add(jcbFontSize);
        for(int i=6;i<=50;i+=2)
     	   jcbFontSize.addItem(i);
-       GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
-				String[] fontnames = e.getAvailableFontFamilyNames();
-				for (int i = 0; i < fontnames.length; i++)
-				jcb.addItem(fontnames[i]);
+       GraphicsEnvironment e =GraphicsEnvironment.getLocalGraphicsEnvironment();
+       String[] fontnames = e.getAvailableFontFamilyNames();
+	for (int i = 0; i < fontnames.length; i++)
+	   jcb.addItem(fontnames[i]);
 				
        mb.add(menu1);
        mb.add(menu2);
        mb.add(menu3);
-       add(mb);
+       j1.add(mb);
+       add(j1,BorderLayout.NORTH);
        }
   public static void main(String[] args)
     {
